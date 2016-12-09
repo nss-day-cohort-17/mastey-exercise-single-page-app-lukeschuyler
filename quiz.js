db2c70726e8ac8732fc45624e8e98a4f7df6b448
Luke Schuyler <lukeschuyler1@gmail.com>
2016-12-09 15:48:17 -0600

    added color and border change on click of cr card divs

--- quiz.js
+++ quiz.js
@@ -1,16 +1,8 @@
 var inventory = [];
 loadInventory();
 
-function activateEvents() {
-
-}
 
 
-function populatePage(inventory) {
-
-    activateEvents();
-
-}
 
 function loadInventory(e) {
     
@@ -20,6 +12,25 @@ function loadInventory(e) {
 
     inventoryLoader.addEventListener("load", function (e) {
 
+    function changeStyles(e) {
+
+        if (e.path[1].className === "row") {
+           console.log(e)
+            e.path[0].classList.add('backgroundChange');
+
+        } else if (e.path[2].className === 'row') {
+
+            console.log(e)
+
+            e.path[1].classList.add('backgroundChange');
+        }
+
+    }
+
+    function defaultStyles(e) {
+
+    }
+
     var data = JSON.parse(e.target.responseText);
 
     console.log(data.cars[0].make);
@@ -28,10 +39,10 @@ function loadInventory(e) {
 
     for (var i = 0; i < data.cars.length; i++) {
                             
-    carDivHTML +=           `<div class= "col-md-4 col-sm-4 col-xs-4 carCard">
+    carDivHTML +=           `<div id="carCards${[i]}" class= "col-md-4 col-sm-4 col-xs-4 carCard">
                                 <span id="makeAndModel">${data.cars[i].make} ${data.cars[i].model}</span>
                                 <p id="carYear[i]" class="carYear">Year: ${data.cars[i].year}</p>
-                                <p id="carPrics[i]" class="carPrice">Year: ${data.cars[i].price}</p>
+                                <p id="carPrics[i]" class="carPrice">Price: $ ${data.cars[i].price}</p>
                             </div>`
                         
             }
@@ -39,6 +50,9 @@ function loadInventory(e) {
             carDivHTML+= `</div>`
 
             document.querySelector('#car-container').innerHTML = carDivHTML;
+
+
+         document.addEventListener('click', changeStyles)
 
     });
 
@@ -50,5 +64,3 @@ inventoryLoader.send();
 function defaultCarStyles(e) {
 
 }
-
-populatePage(inventory)
