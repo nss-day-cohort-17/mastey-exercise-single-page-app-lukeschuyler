ef5fc5f470f0e12a12fe530cba40bf751f1bc080
Luke Schuyler <lukeschuyler1@gmail.com>
2016-12-10 17:04:00 -0600

    made individual divs change color unique

--- quiz.js
+++ quiz.js
@@ -1,51 +1,109 @@
-var inventory = [];
+
 loadInventory();
 
 var styleChangeBoolian = false;
 
 
-function loadInventory(e) {
+function loadInventory() {
     
     var inventoryLoader = new XMLHttpRequest();
 
     // load event listener
 
-    inventoryLoader.addEventListener("load", function (e) {
+    inventoryLoader.addEventListener("load", function(e) {
+
+
+
+     
+
+
+
 
 // CHANGE CARD STYLE FUNCTION
 
     function changeStyles(e) {
 
-        if (e.path[1].className === "row") {
+        var backgroundCards = document.getElementsByClassName('backgroundChange');
 
-            e.path[0].classList.add('backgroundChange');
+        // console.log(backgroundCards.length);
+
+        for (var i = 0; i < backgroundCards.length;i++) {
+            backgroundCards[i].setAttribute('class', 'col-md-4 col-sm-4 col-xs-4 carCard');
+        }
+
+
+        if (e.path[1].className === "row") {
+            // console.log(e);     
+
+            e.path[0].classList.toggle('backgroundChange');
             
         } else if (e.path[2].className === 'row') {
 
-            e.path[1].classList.add('backgroundChange');
+            e.path[1].classList.toggle('backgroundChange');
             
         }
 
-    }
+}
 
-    function changeDescription (e) {
+    
+
+    function editDescription (e) {
         if (e.path[1].className === "row") {
 
             document.querySelector('#edit-input').focus();
+            
             document.querySelector('#edit-input').value = e.path[0].children[3].innerText;
 
+            e.path[0].children[3].innerText = document.querySelector('#edit-input').value;
+
+            console.log(e)
+
         } else if (e.path[2].className === 'row') {
 
             document.querySelector('#edit-input').focus();
 
-            document.querySelector('#edit-input').value = e.path[1].children[3].innerText;            
+            document.querySelector('#edit-input').value = e.path[1].children[3].innerText;     
+
+            e.path[1].children[3].innerText = document.querySelector('#edit-input').value;
+
+            
+        }
+
+    }
+
+    function newDescription(evt) {
+            var key = evt.which || evt.keyCode;
+            if (key === 13) { 
+            
+            var selectedDiv = document.querySelector('.backgroundChange');
+            
+            selectedDiv.children[3].innerText = document.querySelector('#edit-input').value
+
+            console.log(selectedDiv.children[3].innerText)
+
+            for (var i = 0; i < backgroundCards.length;i++) {
+            backgroundCards[i].setAttribute('class', 'col-md-4 col-sm-4 col-xs-4 carCard');
+        }
+
+
         }
     }
+
+    function newDescriptionLive (e) {
+            var selectedDiv = document.querySelector('.backgroundChange');
+            
+            selectedDiv.children[3].innerText = document.querySelector('#edit-input').value
+
+            console.log(selectedDiv.children[3].innerText)
+    }
+
+
+
 
     var data = JSON.parse(e.target.responseText);
 
     var carDivHTML =    `<div class='row'>`
-
+    window.data = data
     for (var i = 0; i < data.cars.length; i++) {
                             
     carDivHTML += `<div id="carCards${[i]}" class= "col-md-4 col-sm-4 col-xs-4 carCard">
@@ -55,17 +113,32 @@ function loadInventory(e) {
                       <p id="carDescription[i]" class="carDescriptions">${data.cars[i].description}</p>
                   </div>`
                         
+            
             }
 
          carDivHTML+= `</div>`
 
-         document.querySelector('#car-container').innerHTML = carDivHTML;
+    document.querySelector('#car-container').innerHTML = carDivHTML;
 
-         document.addEventListener('click', changeStyles);
+    document.addEventListener('click', changeStyles);
 
-         // document.addEventListener('click', defaultStyles);
+    // document.addEventListener('click', defaultStyles);
 
-         document.addEventListener('click', changeDescription)
+    document.addEventListener('click', editDescription);
+
+    document.querySelector('#edit-input').addEventListener('keypress', newDescription)
+
+    document.querySelector('#edit-input').addEventListener('change', newDescriptionLive)
+
+
+
+
+
+
+
+
+
+  
 
 
     });
@@ -74,6 +147,7 @@ inventoryLoader.open('GET', 'inventory.json')
 inventoryLoader.send();
 
 }
+
 
 function defaultCarStyles(e) {
 
