2c052a2269a91048741fdb20121196395bc56077
Luke Schuyler <lukeschuyler1@gmail.com>
2016-12-10 10:05:45 -0600

    added focus to the input bar onclick of the cards, changed some styling for cards

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
 var inventory = [];
 loadInventory();
 
-
+var styleChangeBoolian = false;
 
 
 function loadInventory(e) {
@@ -12,47 +12,63 @@ function loadInventory(e) {
 
     inventoryLoader.addEventListener("load", function (e) {
 
+// CHANGE CARD STYLE FUNCTION
+
     function changeStyles(e) {
 
         if (e.path[1].className === "row") {
-           console.log(e)
+           //  styleChangeBoolian = true;
+           // console.log(e)
             e.path[0].classList.add('backgroundChange');
+            
 
         } else if (e.path[2].className === 'row') {
 
-            console.log(e)
-
             e.path[1].classList.add('backgroundChange');
+            
         }
 
     }
 
-    function defaultStyles(e) {
+    function changeDescription (e) {
+        if (e.path[1].className === "row") {
+           //  styleChangeBoolian = true;
+           // console.log(e)
+            document.querySelector('#edit-input').focus();
+            
 
+        } else if (e.path[2].className === 'row') {
+
+             document.querySelector('#edit-input').focus();
+            
+        }
     }
 
     var data = JSON.parse(e.target.responseText);
-
-    console.log(data.cars[0].make);
 
     var carDivHTML =    `<div class='row'>`
 
     for (var i = 0; i < data.cars.length; i++) {
                             
-    carDivHTML +=           `<div id="carCards${[i]}" class= "col-md-4 col-sm-4 col-xs-4 carCard">
-                                <span id="makeAndModel">${data.cars[i].make} ${data.cars[i].model}</span>
-                                <p id="carYear[i]" class="carYear">Year: ${data.cars[i].year}</p>
-                                <p id="carPrics[i]" class="carPrice">Price: $ ${data.cars[i].price}</p>
-                            </div>`
+    carDivHTML += `<div id="carCards${[i]}" class= "col-md-4 col-sm-4 col-xs-4 carCard">
+                      <span id="makeAndModel">${data.cars[i].make} ${data.cars[i].model}</span>
+                      <p id="carYear[i]" class="carYear">Year: ${data.cars[i].year}</p>
+                      <p id="carPrice[i]" class="carPrice">Price: $ ${data.cars[i].price}</p>
+                      <p id="carDescription[i]" class="carDescriptions">${data.cars[i].description}</p>
+                  </div>`
                         
             }
 
-            carDivHTML+= `</div>`
+         carDivHTML+= `</div>`
 
-            document.querySelector('#car-container').innerHTML = carDivHTML;
+         document.querySelector('#car-container').innerHTML = carDivHTML;
 
+         document.addEventListener('click', changeStyles);
 
-         document.addEventListener('click', changeStyles)
+         // document.addEventListener('click', defaultStyles);
+
+         document.addEventListener('click', changeDescription)
+
 
     });
 
